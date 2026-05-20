af9693edb7511e6b6e50ba1c1a49649392237934
Suresh Babu <suresh.babu@smnetserv.com>
2026-05-20 12:38:14 +0530

    Removed inferred schema , as everything taking as string

--- notebooks/01_bronze_ingestion.py
+++ notebooks/01_bronze_ingestion.py
@@ -8,11 +8,11 @@ spark = SparkSession.builder.getOrCreate()
 
 BASE_PATH = "/Workspace/Users/suresh.babu@accionlabs.com/healthcare-databricks-project"
 
-patients_df = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(f"{BASE_PATH}/data/patients.csv")
+patients_df = spark.read.format("csv").option("header", "true").load(f"{BASE_PATH}/data/patients.csv")
 
-claims_df = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(f"{BASE_PATH}/data/claims.csv")
+claims_df = spark.read.format("csv").option("header", "true").load(f"{BASE_PATH}/data/claims.csv")
 
-payments_df = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(f"{BASE_PATH}/data/payments.csv")
+payments_df = spark.read.format("csv").option("header", "true").load(f"{BASE_PATH}/data/payments.csv")
 
 patients_df.write.mode('overwrite').saveAsTable("workspace.default.bronze_patients")
 claims_df.write.mode('overwrite').saveAsTable("workspace.default.bronze_claims")
